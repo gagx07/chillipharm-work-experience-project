b35731c59b4acfe0ccea3538fa8bb3be937aadad
gagx07 <127550459+gagx07@users.noreply.github.com>
2024-07-03 12:29:11 +0100

    adding function to buttons(incomplete)

--- actions.js
+++ actions.js
@@ -1,3 +1,16 @@
-const button = document.getElementById('click-button-1');
-console.log(button);
-button.addEventListener('click', () => alert('BUTTON CLICKED'));
+document.addEventListener("DOMContentLoaded", (event) => {
+    const button1 = document.getElementById("button1");
+    const button2 = document.getElementById("button2");
+    const button3 = document.getElementById("button3");
+    const box = document.querySelector("button1.box");
+
+    button1.addEventListener('click', () => {
+        box.style.setProperty('--main-color', '#FFEFA8');
+    });
+    button1.addEventListener('click', () => {
+        box.style.setProperty('--main-color', '#8A9A5B');
+    });
+    button1.addEventListener('click', () => {
+        box.style.setProperty('--main-color', '#007BFF');
+    });
+})
